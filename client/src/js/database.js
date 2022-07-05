b9e484947c7ae8b9ffb343d0d06fcc6590e9bf55
Andrew Gavegan <zzandrewzz@optusnet.com.au>
2022-07-05 16:35:04 +1000

    populated database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,36 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('Put to the database');
+  // creating the connection
+  const Db = await openDB('jate', 1);
+  // create a transaction and data privileges 
+  const tx = Db.transaction('jate', 'readwrite');
+  // open object store
+  const store = tx.objectStore('jate');
+  // use add method on the store
+  const request = store.put({ id: id, jate: content });
+  // confirmation message
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from the database');
+
+  const Db = await openDB('jate', 1);
+
+  const tx = Db.transaction('jate', 'readonly');
+
+  const store = tx.objectStore('jate');
+  // use getall method to get all the databases data //
+  const request = store.getAll();
+
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 
 initdb();
